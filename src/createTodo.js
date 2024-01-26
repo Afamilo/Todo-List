5f515f9f4edbcd977bb30989292b3429169a98f4
Afamilo <nichomartins@gmail.com>
2024-01-26 01:48:38 -0600

    initialized and empty array TodoOj function

---
+++ src/createTodo.js
@@ -0,0 +1,9 @@
+let Todo = []; 
+
+function TodoObJ(title, details, dueDate, priority) {
+    this.title = title;
+    this.details = details;
+    this.dueDate = dueDate;
+    this.priority = priority;
+}
+
