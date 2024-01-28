97e68d31f3a0b90e4ab69c970a20842e55efecb3
Afamilo <nichomartins@gmail.com>
2024-01-28 01:12:07 -0600

    adding a getTodoElement function

--- src/createTodo.js
+++ src/createTodo.js
@@ -13,3 +13,37 @@ function clearForm() {
     dueDate.value = "";
     priority.value = "";
 }
+
+function addTodoToList() {
+    let title = document.querySelector("#todo-Title").value;
+    let details = document.querySelector("#todo-Details").value;
+    let dueDate = document.querySelector("#todo-Date").value;
+    let priority = document.querySelector("#todo-Priority").value;
+    let newTodo = new TodoOb(title, details, dueDate, priority);
+    Todo.push(newTodo);
+    renderOnScreen();
+}
+
+export default addTodoToList();
+
+function getTodoEelement(todo) {
+    const getTodoEelement = document.createElement("li");
+    getTodoEelement.classList.add("todo-item");
+
+    const todoTitle = getTodoTitle(todo);
+    const todoDetails = getTodoDetails(todo);
+    const todoDueDate = getDueDate(todo);
+    const todoPriority = getTodoPriority(todo);
+    const completedBtn = getCompletedTodoBtn(todo);
+    const deleteTodoBtn = getDeleteTodoBtn(todo);
+
+    todoElement.appendChild(todoTitle);
+    todoElement.appendChild(todoDetails);
+    todoElement.appendChild(todoDueDate);
+    todoElement.appendChild(todoPriority);
+    todoElement.appendChild(completedBtn);
+    todoElement.appendChild(deleteTodoBtn);
+
+    return todoElement;
+}
+
