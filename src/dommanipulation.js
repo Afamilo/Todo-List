38ba75e61d50e6cb96b717db06f6afb5c50d101a
Afamilo <nichomartins@gmail.com>
2024-01-29 23:08:05 -0600

    importing addTodoList function and exporting submit-= btn

---
+++ src/dommanipulation.js
@@ -0,0 +1,10 @@
+import { addTodoToList } from "./createTodo";
+
+const submitTodoBtn = document.querySelector("#submit-button")
+
+export function submitTodo() {
+    submitTodoBtn.addEventListener("click", () => {
+        addTodoToList();
+        
+    })
+}
